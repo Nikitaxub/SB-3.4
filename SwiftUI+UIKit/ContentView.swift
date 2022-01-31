de727824f1b1967c86ffebd36f6960746278e148
nikita <nikita.dmd@gmail.com>
2022-01-31 22:29:57 +0300

    ready

--- SwiftUI+UIKit/ContentView.swift
+++ SwiftUI+UIKit/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentValue: Float = Float.random(in: 0...100)
+    @State private var isChecked: Bool = false
+    @State private var targetValue: Int = Int.random(in: 0...100)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
+                .padding()
+            HStack {
+                Text("0")
+                CurrentValueSlider(value: $currentValue, opacity: computeScore())
+                Text("100")
+            }
+            Button("Проверь меня!", action: { isChecked = true })
+                .alert("Your score", isPresented: $isChecked, actions: {}) {
+                    Text("\(computeScore())")
+                }
+                .padding()
+            Button("Начать заново", action: changeValue)
+        }
+        
+    }
+    
+    private func changeValue() {
+        targetValue = Int.random(in: 0...100)
+    }
+
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(Double(currentValue)))
+        return 100 - difference
     }
 }
 
---
+++ SwiftUI+UIKit/CurrentValueSlider.swift
@@ -0,0 +1,57 @@
+//
+//  CurrentValueSlider.swift
+//  SwiftUI+UIKit
+//
+//  Created by xubuntus on 31.01.2022.
+//
+
+import SwiftUI
+
+struct CurrentValueSlider: UIViewRepresentable {
+    @Binding var value: Float
+    let opacity: Int
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.thumbTintColor = UIColor(red: 1, green: 0, blue: 0, alpha: Double(opacity) / 100)
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged)
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = value
+        uiView.thumbTintColor = UIColor(red: 1, green: 0, blue: 0, alpha: Double(opacity) / 100)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(sliderValue: $value)
+    }
+}
+
+extension CurrentValueSlider {
+    class Coordinator: NSObject {
+        @Binding var sliderValue: Float
+        
+        init(sliderValue: Binding<Float>) {
+            self._sliderValue = sliderValue
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            sliderValue = sender.value
+        }
+    }
+}
+
+struct CurrentValueSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentValueSlider(value: .constant(50), opacity: 50)
+    }
+}
