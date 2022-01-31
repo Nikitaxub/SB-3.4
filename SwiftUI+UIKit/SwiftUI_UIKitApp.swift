45331d27ca593898a4c720fe38a48282496324c5
nikita <nikita.dmd@gmail.com>
2022-01-31 13:47:18 +0300

    Initial Commit

---
+++ SwiftUI+UIKit/SwiftUI_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIKitApp.swift
+//  SwiftUI+UIKit
+//
+//  Created by xubuntus on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
